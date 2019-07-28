04e12ee1a54378897ef8b06ff531f839b25076d2
Tierra <sr.undershaft@gmail.com>
2019-07-28 15:25:51 -0500

    initalize html js and firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC7SjZGgTfEsSp37XjIY4S9pVHfoBzzlNM",
+    authDomain: "rps-game-e277c.firebaseapp.com",
+    databaseURL: "https://rps-game-e277c.firebaseio.com",
+    projectId: "rps-game-e277c",
+    storageBucket: "",
+    messagingSenderId: "72373962536",
+    appId: "1:72373962536:web:f617a4016adff89c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
